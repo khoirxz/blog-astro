c85b69ec3b39302ede09d05629beed768f839131
khoirxz <khoirrurrohman@gmail.com>
2023-07-07 21:05:52 +0700

    i hope this will work

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,12 +2,12 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_DESC, SITE_TITLE } from "../consts";
 
-export async function get(context) {
+export async function get() {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE || "",
     description: SITE_DESC || "",
-    site: context.site,
+    site: "https://rizqik-blog.vercel.app//",
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}`,
