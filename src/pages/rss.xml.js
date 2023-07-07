37a16dc5be78df213b77f309b310f89f80ffd3ac
khoirxz <khoirrurrohman@gmail.com>
2023-07-07 20:39:00 +0700

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,9 +5,9 @@ import { SITE_DESC, SITE_TITLE } from "../consts";
 export async function get(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESC,
-    site: context.site,
+    title: SITE_TITLE || "",
+    description: SITE_DESC || "",
+    site: context.site || "",
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}`,
