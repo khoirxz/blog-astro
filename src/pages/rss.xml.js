8a6f2e78bc6dc94f81d9e95430a4a70444a6110d
khoirxz <khoirrurrohman@gmail.com>
2023-07-07 19:32:54 +0700

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import { SITE_DESC, SITE_TITLE } from "../consts";
 
 export async function get(context) {
   const posts = await getCollection("blog");
   return rss({
+    title: SITE_TITLE,
+    description: SITE_DESC,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
